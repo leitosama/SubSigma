f2452ad3e72940b537be5f50538068d43a14faa6
leitosama <fs.leito@gmail.com>
2023-06-09 12:21:25 +0000

    JSON config init

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,10 +12,14 @@ import (
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
+type Config struct {
+	Repos []Repostitory `json:"repos"`
+}
+
 type Repostitory struct {
-	addr      string
-	lasthash  string
-	rulespath string
+	Addr      string `json:"addr"`
+	Lasthash  string `json:"lasthash"`
+	Rulespath string `json:"rulespath"`
 }
 
 func normalize_path(dirtypath string) string {
@@ -22,14 +27,31 @@ func normalize_path(dirtypath string) string {
 	return strings.TrimPrefix(string(p), "/") + "/"
 }
 func main() {
-	// TODO: Parse config to repos
-	repos := []Repostitory{
-		{addr: "https://github.com/joesecurity/sigma-rules/", lasthash: "39c5f36034e12ca81a7a4d835889dfb07c0b3903", rulespath: normalize_path("rules")},
-		{addr: "https://github.com/SigmaHQ/sigma", lasthash: "c5c61ac04052632889999f21f96ddbec9efa2219", rulespath: "rules"},
+	config := `{
+		"repos": [{
+				"addr": "https://github.com/joesecurity/sigma-rules/",
+				"lasthash": "39c5f36034e12ca81a7a4d835889dfb07c0b3903",
+				"rulespath": "rules"
+			},
+			{
+				"addr": "https://github.com/SigmaHQ/sigma",
+				"lasthash": "c5c61ac04052632889999f21f96ddbec9efa2219",
+				"rulespath": "rules"
+			}
+		]
 	}
-	for _, repo := range repos {
+	`
+	cfg := Config{}
+	err := json.Unmarshal([]byte(config), &cfg)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(cfg)
+	// TODO: Parse config to repos
+	for _, repo := range cfg.Repos {
 		objrepo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-			URL: repo.addr,
+			URL: repo.Addr,
 		})
 		if err != nil {
 			fmt.Println("Error opening repository:", err)
@@ -44,7 +66,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		scommit, err := objrepo.CommitObject(plumbing.NewHash(repo.lasthash))
+		scommit, err := objrepo.CommitObject(plumbing.NewHash(repo.Lasthash))
 		if err != nil {
 			fmt.Println("Error getting scommit:", err)
 			os.Exit(1)
@@ -55,7 +77,7 @@ func main() {
 		}
 		for _, el := range patch.FilePatches() {
 			oldfile, newfile := el.Files()
-			if oldfile == nil && strings.HasPrefix(newfile.Path(), repo.rulespath) {
+			if oldfile == nil && strings.HasPrefix(newfile.Path(), repo.Rulespath) {
 				fmt.Println(newfile.Path())
 			}
 		}
